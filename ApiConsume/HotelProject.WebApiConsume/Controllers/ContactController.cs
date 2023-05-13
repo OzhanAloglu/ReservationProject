308a545ef865f16d44a8dcd69b98966fa6af3f96
Özhan <ozhanaloglu1@gmail.com>
2023-05-13 22:22:53 +0300

    .

---
+++ ApiConsume/HotelProject.WebApiConsume/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApiConsume.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        
+        {
+            _contactService = contactService;
+        }
+
+        [HttpPost]
+
+        public IActionResult AddService(Contact contact)
+        {
+            contact.Date=Convert.ToDateTime(DateTime.Now.ToString());
+            _contactService.TInsert(contact);
+            return Ok();
+        }
+    }
+}
