12c5e736805faad2b370430c8f49ffef8f9f1b1f
Özhan <ozhanaloglu1@gmail.com>
2023-05-23 23:22:20 +0300

    .

--- ApiConsume/HotelProject.WebApiConsume/Controllers/ContactController.cs
+++ ApiConsume/HotelProject.WebApiConsume/Controllers/ContactController.cs
@@ -19,11 +19,19 @@ namespace HotelProject.WebApiConsume.Controllers
 
         [HttpPost]
 
-        public IActionResult AddService(Contact contact)
+        public IActionResult AddContact(Contact contact)
         {
             contact.Date=Convert.ToDateTime(DateTime.Now.ToString());
             _contactService.TInsert(contact);
             return Ok();
+        }
+
+
+        [HttpGet]
+        public IActionResult InboxListContact()
+        {
+            var values = _contactService.TGetList();
+            return Ok(values);
         }
     }
 }
--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.SendMessageDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class AdminContactController : Controller
     {
-        public IActionResult Inbox()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            //Api Consume işlemi böyle yapılır. Bunu yapmadan önce, API katmanında Program.cs içinde gerekli konfigürasyonları yapmak gerekiyor. 
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21771/api/Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> SendBox()
+        {
+ 
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21771/api/SendMessage");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSendBoxDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddSendMessage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSendMessage(CreateSendMessage createSendMessage)
+        {
+            createSendMessage.SenderMail = "admin@gmail.com";
+            createSendMessage.SenderName = "admin";
+            createSendMessage.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSendMessage);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:21771/api/SendMessage", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SendBox");
+            }
+
+            return View();
+        }
+
+
+        public PartialViewResult SideBarAdminContactPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SideBarAdminContactCategoryPartial()
+        {
+            return PartialView();
         }
     }
 }
