54eebf928cab54601eefa5032fad221a1f253067
Özhan <ozhanaloglu1@gmail.com>
2023-06-09 21:38:51 +0300

    .

--- ApiConsume/HotelProject.WebApiConsume/Controllers/BookingController.cs
+++ ApiConsume/HotelProject.WebApiConsume/Controllers/BookingController.cs
@@ -70,5 +70,12 @@ namespace HotelProject.WebApiConsume.Controllers
             _bookingService.BookingStatusChangeApproved2(id);
             return Ok();
         }
+
+        [HttpGet("Last6Booking")]
+        public IActionResult Last6Booking()
+        {
+          var values=  _bookingService.TLast6Bookings();
+            return Ok(values);
+        }
     }
 }
---
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardLast6Bookings.cs
@@ -0,0 +1,35 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.Dashboard
+{
+    public class _DashboardLast6Bookings:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast6Bookings(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Api Consume işlemi böyle yapılır. Bunu yapmadan önce, API katmanında Program.cs içinde gerekli konfigürasyonları yapmak gerekiyor. 
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:21771/api/Booking/Last6Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast6BookingDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
